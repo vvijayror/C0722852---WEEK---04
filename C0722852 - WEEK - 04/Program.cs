aca50807ff6ff63a022870898bcdadb2da851ac6
visualstudio <visualstudio@122-PC21>
2019-01-30 10:21:27 -0500

    commit

--- C0722852 - WEEK - 04/Program.cs
+++ C0722852 - WEEK - 04/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿=using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +7,14 @@ using System.Threading.Tasks;
 namespace C0722852___WEEK___04
 {
     class Program
+
+    class Village
     {
-        static void Main(string[] args)
-        {
-        }
+        // Node is an ADT
+        // what KIND of DATA do we need in a NODE?
+        public Village nextVillage;
+        public String VillageName;
+        public bool idAstrildeHere = false;
+        
     }
 }
